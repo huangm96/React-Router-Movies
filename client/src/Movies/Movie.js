ed86e3e2a388ba6ec367b9ec7cd6d937b2eed7b0
Min Huang <minh.huang96@gmail.com>
2019-08-20 13:02:05 -0700

    finished MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieList from './MovieList';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
+    useEffect(() => {
+      const subscription = props.source.subscribe();
+      return () => {
+        // Clean up the subscription
+        subscription.unsubscribe();
+      };
+    });
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -13,6 +21,7 @@ const Movie = (props) => {
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
+          
         })
         .catch(error => {
           console.error(error);
